129d8c0bfbf54772071b9cd5a32271f39379b0d8
Christian Brüggemann <mail@cbruegg.com>
2023-03-31 17:35:46 +0200

    Make compilation more robust

--- build-cbruegg-mac.ps1
+++ build-cbruegg-mac.ps1
@@ -1,4 +1,5 @@
 # Currently this project does not build with Node 19+
 $env:PATH = "/opt/homebrew/opt/node@18/bin/:"  + $env:PATH;
 
-yarn install && yarn build:chrome
+rm -r node_modules && yarn install && yarn build:chrome
+echo "If this produced no output files, run 'yarn dev:chrome' for more verbose messages."
