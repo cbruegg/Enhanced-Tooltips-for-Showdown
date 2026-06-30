e23456107a64d2b1997fd7e4c806473493055d39
cbruegg <mail@cbruegg.com>
2026-06-30 19:22:28 +0200

    Convert build-cbruegg-mac.ps1 to bash

--- build-cbruegg-mac.sh
+++ build-cbruegg-mac.sh
@@ -1,5 +1,11 @@
-# Currently this project does not build with Node 19+
-$env:PATH = "/opt/homebrew/opt/node@18/bin/:"  + $env:PATH;
+#!/usr/bin/env bash
+set -euo pipefail
 
-rm -r node_modules && yarn install && yarn build:chrome
+# Currently this project does not build with Node 19+
+export PATH="/opt/homebrew/opt/node@18/bin:$PATH"
+
+rm -rf node_modules
+yarn install
+yarn build:chrome
+
 echo "If this produced no output files, run 'yarn dev:chrome' for more verbose messages."
